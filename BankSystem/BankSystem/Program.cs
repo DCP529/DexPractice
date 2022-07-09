9b063d7fa74bce2cdaa5036b3e538d26758691f1
DCP529 <61003286+DCP529@users.noreply.github.com>
2022-07-09 21:06:34 +0300

    return to work

--- BankSystem/BankSystem/Program.cs
+++ BankSystem/BankSystem/Program.cs
@@ -1,39 +1,48 @@
 ﻿using BankSystem.Models;
 using BankSystem.Services;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+using System.Data;
 using System;
-using System.IO;
+using System.Threading.Tasks;
 
 namespace BankSystem
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            BankServices bankServices = new BankServices();
-            Exchange exchange = new Exchange();
+            //BankServices bankServices = new BankServices();
+            //Exchange exchange = new Exchange();
 
-            string path = Path.Combine("C:", "Users", "37377", "Documents", "GitHub", "DexPractice", "BankSystem", "DataPerson");
+            //string path = Path.Combine("C:", "Users", "37377", "Documents", "GitHub", "DexPractice", "BankSystem", "DataPerson");
 
-            Account account1 = new Account() { Money = 15_000, CurrencyType = new Rub() };
-            Account account2 = new Account() { Money = 15_000, CurrencyType = new Ua() };
+            //Account account1 = new Account() { Money = 15_000, CurrencyType = new Rub() };
+            //Account account2 = new Account() { Money = 15_000, CurrencyType = new Ua() };
 
-            Client client1 = new Client() { Name = "Ben", Age = 10, Passport = 1 };
-            Client client2 = new Client() { Name = "Benis", Age = 10, Passport = 2 };
+            //Client client1 = new Client() { Name = "Ben", Age = 10, Passport = 1 };
+            //Client client2 = new Client() { Name = "Benis", Age = 10, Passport = 2 };
 
-            Employee employee = new Employee() { Name = "Gof", Age = 15, Passport = 2 };
-            var exchangeHandler = new Func<decimal, Currency, Currency, decimal>(exchange.Converter);
+            //Employee employee = new Employee() { Name = "Gof", Age = 15, Passport = 2 };
+            //var exchangeHandler = new Func<decimal, Currency, Currency, decimal>(exchange.Converter);
 
-            bankServices.AddClientAccount(client1, account2);
-            bankServices.AddClientAccount(client1, account1);
-            //bankServices.MoneyTransfer(10_000, account1, account2, exchangeHandler);
+            //bankServices.AddClientAccount(client1, account2);
+            //bankServices.AddClientAccount(client1, account1);
+            ////bankServices.MoneyTransfer(10_000, account1, account2, exchangeHandler);
 
-            bankServices.Add<IPerson>(client2);
+            //bankServices.Add<IPerson>(client2);
             //bankServices.Add<IPerson>(employee);
 
-            var result1 = bankServices.FindClient(client2);
+            //var result1 = bankServices.FindClient(client2);
             //var result2 = bankServices.FindEmployee(employee);
 
-            var dictionary = bankServices.CreateDictionaryFromFile(path, "Dictionary");
+            //var dictionary = bankServices.CreateDictionaryFromFile(path, "Dictionary");
+
+
+            //DataExport de = new DataExport();
+            //de.AddDataToFile<Account>(account1, Path.Combine("C:", "Users", "37377", "Documents", "GitHub", "DexPractice", "BankSystem", "DataPerson"));
+
+            
 
             Console.ReadLine();
         }
---
+++ BankSystem/BankSystem/Services/DataExport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace BankSystem.Services
+{
+    public class DataExport
+    {
+        public void AddDataToFile<T>(T obj, string path)
+        {
+            var type = obj.GetType();
+            var properties = type.GetProperties();
+
+            using (FileStream fs = new FileStream(path, FileMode.Append))
+            {
+                var serializeProp = JsonConvert.SerializeObject(properties);
+
+                byte[] dataArray = Encoding.Default.GetBytes(serializeProp);
+                fs.Write(dataArray);
+            }
+        }
+    }
+}
